b6c1ae8252ec01edc6c765c18cf721451fb7cc79
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2016-01-02 00:38:07 +0000

    add logarithmic spiral option and additional parameters

--- curlicue.py
+++ curlicue.py
@@ -28,7 +28,7 @@ bl_info = \
     {
         "name" : "Curlicue",
         "author" : "Lawrence D'Oliveiro <ldo@geek-central.gen.nz>",
-        "version" : (0, 1, 0),
+        "version" : (0, 2, 0),
         "blender" : (2, 7, 6),
         "location" : "Add > Mesh > Curlicue",
         "description" :
@@ -53,6 +53,17 @@ class Curlicue(bpy.types.Operator) :
     bl_context = "objectmode"
     bl_options = {"REGISTER", "UNDO"}
 
+    curve_type = bpy.props.EnumProperty \
+      (
+        name = "Type",
+        description = "the type of spiral curve",
+        items =
+            (
+                ("linear", "Linear", "linear spiral"),
+                ("log", "Logarithmic", "logarithmic spiral"),
+            ),
+        default = "linear",
+      )
     nr_points = bpy.props.IntProperty \
       (
         name = "Points",
@@ -87,26 +98,64 @@ class Curlicue(bpy.types.Operator) :
         min = 0,
         default = 0.0,
       )
+    linear_offset = bpy.props.FloatProperty \
+      (
+        name = "Linear Offset",
+        description = "the starting radius of the innermost end of the spiral (linear only)",
+        min = 0,
+        default = 0.0,
+      )
+    radius_ratio = bpy.props.FloatProperty \
+      (
+        name = "Radius Ratio",
+        description = "the ratio of the radii of successive full turns (logarithmic only)",
+        min = 0,
+        default = 1.0,
+      )
 
     def draw(self, context) :
         the_col = self.layout.column(align = True)
         #the_col.label("Curlicue:") # Blender already labels redo panel for me
+        the_col.prop(self, "curve_type")
         the_col.prop(self, "nr_points")
         the_col.prop(self, "nr_turns")
         the_col.prop(self, "radius")
         the_col.prop(self, "inner_taper")
         the_col.prop(self, "outer_taper")
+        the_col.prop(self, "linear_offset")
+        the_col.prop(self, "radius_ratio")
     #end draw
 
     def action_common(self, context, redoing) :
         try :
-            curve_name = "SpiralMesh"
+            curve_name = "Curlicue"
             vertices = []
             faces = []
             for i in range(self.nr_points) :
                 angle = i / self.nr_points * self.nr_turns * 2 * math.pi
-                radius = i / self.nr_points * self.radius
-                width = self.outer_taper * i / self.nr_points + self.inner_taper * (1 - i / self.nr_points)
+                if self.curve_type == "linear" :
+                    radius = \
+                        (
+                            i / self.nr_points * (self.radius - self.linear_offset)
+                        +
+                            self.linear_offset
+                        )
+                elif self.curve_type == "log" :
+                    radius = \
+                        (
+                            self.radius
+                        *
+                            self.radius_ratio ** (angle / (2 * math.pi) - self.nr_turns)
+                        )
+                else :
+                    raise Failure("SHOULDN’T OCCUR: unrecognized curve type")
+                #end if
+                width = \
+                    (
+                        self.outer_taper * i / self.nr_points
+                    +
+                        self.inner_taper * (1 - i / self.nr_points)
+                    )
                 to_midpoint = \
                     (
                         Matrix.Rotation(angle, 4, "Z")
